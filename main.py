ba1ff4e4a9858ed61217b1a34ac902ecd314fa25
cauafreitas7 <128155396+cauafreitas7@users.noreply.github.com>
2023-05-03 10:58:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-cont = 0
-número =int(input("Digite um número: "))
-if número < 0 or número == 0:
-   print("Número Inválido")
-else:
-  for primos in range(1, número+1):
-     if (número % primos) == 0:
-        cont += 1
-  if cont > 2 or número == 1:
-     print("Não é primo")
-  else:
-    print("É primo")
+numero = input("digite um número inteiro")
+numero = int(numero)
+a = 1
+while True:
+    a = a + 1
+    if numero == 1:
+        print("Não primo")
+        break
+    elif numero <= 0:
+        print("Número inválido")
+        break
+    elif numero % a == 0 and a != numero:
+        print("Não primo")
+        break
+    elif a == numero:
+        print("Primo")
+        break
